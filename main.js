eba67bcf0a499c1551ed55a545dc9aa475251cad
danielevergani <dvergani94@gmail.com>
2021-03-12 15:51:35 +0100

    cambio foto usando funzioni

--- main.js
+++ main.js
@@ -1,32 +1,39 @@
 $(function(){
 
     $(".prev").click(
-        function(){
-            var $photo = $(".images img.active");
-            if($photo.hasClass("first")){
-                $photo.removeClass("active")
-                $(".images img.last").addClass("active")
-            }
-            else{
-                $photo.removeClass("active")
-                $photo.prev().addClass("active")
-            }
-            
-        }
+        function (){
+            prevPhoto();
+        } 
     );
 
     $(".next").click(
         function(){
-            var $photo = $(".images img.active");
-            if($photo.hasClass("last")){
-                $photo.removeClass("active")
-                $(".images img.first").addClass("active")
-            }
-            else{
-                $photo.removeClass("active")
-                $photo.next().addClass("active")
-            }
+            nextPhoto();
         }
     );
 }
 );
+
+function prevPhoto(){
+    var $photo = $(".images img.active");
+    if($photo.hasClass("first")){
+        $photo.removeClass("active")
+        $(".images img.last").addClass("active")
+    }
+    else{
+        $photo.removeClass("active")
+        $photo.prev().addClass("active")
+    }
+}
+
+function nextPhoto(){
+    var $photo = $(".images img.active");
+    if($photo.hasClass("last")){
+        $photo.removeClass("active")
+        $(".images img.first").addClass("active")
+    }
+    else{
+        $photo.removeClass("active")
+        $photo.next().addClass("active")
+    }
+}
