1682aeaa3c6142d1358516d99ba0eaded45adb34
danielevergani <dvergani94@gmail.com>
2021-03-12 16:28:36 +0100

    pallini si muovono coerentemente a img che scorrono

--- main.js
+++ main.js
@@ -11,29 +11,40 @@ $(function(){
             nextPhoto();
         }
     );
+
 }
 );
 
 function prevPhoto(){
     var $photo = $(".images img.active");
+    var $circle = $(".nav i.active");
     if($photo.hasClass("first")){
         $photo.removeClass("active")
         $(".images img.last").addClass("active")
+        $circle.removeClass("active")
+        $(".nav i.last").addClass("active")
     }
     else{
         $photo.removeClass("active")
         $photo.prev().addClass("active")
+        $circle.removeClass("active")
+        $circle.prev().addClass("active")
     }
 }
 
 function nextPhoto(){
     var $photo = $(".images img.active");
+    var $circle = $(".nav i.active");
     if($photo.hasClass("last")){
         $photo.removeClass("active")
         $(".images img.first").addClass("active")
+        $circle.removeClass("active")
+        $(".nav i.first").addClass("active")
     }
     else{
         $photo.removeClass("active")
         $photo.next().addClass("active")
+        $circle.removeClass("active")
+        $circle.next().addClass("active")
     }
 }
